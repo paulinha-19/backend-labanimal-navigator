7e0ce6645730464fda98514d497541fb518a9d16
Paulinha <paulafabianasoares@gmail.com>
2025-01-09 01:53:35 -0300

    [fix] production knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,23 +1,8 @@
 require("dotenv").config();
 
-const env = process.env.AUTHORIZATION || process.env.NODE_ENV || "development";
+const env = process.env.AUTHORIZATION || process.env.NODE_ENV || "production";
 
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      host: process.env.PGHOST,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-      database: process.env.PGDATABASE,
-      port: process.env.PGPORT || 5432,
-    },
-    migrations: {
-      extension: "ts",
-      tableName: "knex_migrations",
-      directory: "./migrations",
-    },
-  },
   production: {
     client: "pg",
     connection: {
