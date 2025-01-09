713185e98cd210b63032fd91410ff4f699c254be
Paulinha <paulafabianasoares@gmail.com>
2025-01-09 17:10:39 -0300

    [fix] connectDb - Error: connect ECONNREFUSED 127.0.0.1:5432

--- src/connectDB.ts
+++ src/connectDB.ts
@@ -1,17 +1,6 @@
 import Knex from "knex";
+import knexConfig from "../knexfile";
 require("dotenv").config();
 
-export const knex = Knex({
-  client: "pg",
-  connection: {
-    host: process.env.PGHOST,
-    port: Number(process.env.PGPORT),
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-  },
-  pool: {
-    min: 1,
-    max: 10,
-  },
-});
+export const knex = Knex(knexConfig)
+
